dcab95fcc29253669d32892dcf5f0a9555d2a867
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-13 14:01:40 -0400

    Adding modal markup to html and creating add card random task id select logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId")) || 1;
 const formModal = document.getElementById('formModal');
+const tasks = [];
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    return nextId ++;
+   return Math.floor(Math.random * 100000).toString();
+  
 }
 
 // Todo: create a function to create a task card
@@ -16,7 +18,21 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    $('#task-list').empty();
+$('#todo-card').html('');
+$('#in-process-cards').html('');
+$('#done-cards').html('');
+
+tasks.forEach((task) => {
+    if(task.status === 'to-do') {
+        $('#todo-card').append(createTaskCard(task, false))
+    if(task.status === 'in-process') {
+        $('#in-process-card').append(createTaskCard(task, false))
+    }
+    if(task.status === 'done') {
+        $('#done-card').append(createTaskCard(task, false))
+    }
+    }
+} )
 }
 
 // Todo: create a function to handle adding a new task
