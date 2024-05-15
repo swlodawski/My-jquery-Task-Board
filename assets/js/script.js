e95586c56ba8d8c48704a68a5203c7b15edeb314
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-15 15:16:41 -0400

    Adding logic to handle a dropped card. Then set new status of the card and set the new status in local strorage and convert the new status of the tasks array to json format.

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,7 +128,18 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+    const taskId = ui.draggable[0].dataset.task;
 
+    const newStatus = event.target.id;
+
+    tasks.forEach((task) => {
+        if(task.id === taskId)
+    {task.status = newStatus;
+    }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify((tasks)));
+    renderTaskList();   
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
